720d8b30be93a19d35ce7e1d75e94ab3ed1ea5f5
Jody Phelan <jody.phelan@lshtm.ac.uk>
2022-03-28 17:56:08 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+version = [l.strip() for l in open("tb_ml/__init__.py") if "version" in l][0].split('"')[1]
+
+setuptools.setup(
+
+	name="tb-ml",
+	version=version,
+	packages=["tb_ml"],
+	license="GPL3",
+	long_description="A standardised approach to predicting resistance using machine learning",
+	scripts=[
+		'scripts/tb-ml-predict.py'
+	]
+)
