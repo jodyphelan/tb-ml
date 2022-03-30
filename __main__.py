b28be7fe2afbca0fe986d3dce65701525e45e4dd
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-03-30 16:08:55 +0100

    add function for resistance prediction

--- __main__.py
+++ __main__.py
@@ -1,10 +1,30 @@
+# %% ###################################################################
 import pandas as pd
 from tb_ml import funcs
+import subprocess
+# %% ###################################################################
 
 
 def main() -> None:
-    pass
+    """
+    Test what we got so far using a mock function for step two (the variant
+    calling).
+    """
+    af_fname: str = 'test_data/SM_training_means.csv'
+    AFs: pd.Series = pd.read_csv(af_fname, index_col=0).squeeze()
+    pos: list[int] = funcs.get_positions(AFs)
+    vars = funcs.TEST_run_variant_calling_pipeline(
+        'test_data/INH_ERR1035006.csv', pos)
+    vars_rdy = funcs.sanitize_input_dimensions(vars, AFs)
+    docker_img_fname = '/home/julian/git/tb-ml/docker/' \
+        'RF_INH_predictor/rf-sm-predictor.docker.tar'
+    res_status: bool = funcs.run_prediction_container(docker_img_fname,
+                                                      vars_rdy)
+    if res_status:
+        print('resistant')
+    else:
+        print('not resistant')
 
 
-if __name__ == 'main':
-    main()
+# if __name__ == 'main':
+main()
--- tb_ml/funcs.py
+++ tb_ml/funcs.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import Comment
 import pandas as pd
+import subprocess
 import argparse
 from typing import Iterable, Union
 
@@ -8,7 +9,7 @@ def get_cli_args() -> argparse.Namespace:
     pass
 
 
-def get_positions(AFs: pd.Series) -> list:
+def get_positions(AFs: pd.Series) -> list[int]:
     """
     Extracts the genomic positions of interest from the AF series; assumes
     the index entries to have the format: 'POS_REF_ALT'.
@@ -17,12 +18,23 @@ def get_positions(AFs: pd.Series) -> list:
     return pos
 
 
-def run_variant_calling_pipeline(seqs_fname: str, pos: Iterable) -> None:
+def run_variant_calling_pipeline(seqfile: str,
+pos: Iterable[int]) -> pd.Series:
     """
     Runs the variant calling pipeline on the raw reads of the sample using a
     vector of positions.
     """
     pass
+
+
+def TEST_run_variant_calling_pipeline(mock_vars_fname: str,
+                                      pos: Iterable[int]) -> pd.Series:
+    """
+    Runs the variant calling pipeline on the raw reads of the sample using a
+    vector of positions.
+    """
+    vars = pd.read_csv(mock_vars_fname, index_col=0).squeeze()
+    return vars
 
 
 def sanitize_input_dimensions(vars: pd.Series, AFs: pd.Series) -> pd.Series:
@@ -31,14 +43,26 @@ def sanitize_input_dimensions(vars: pd.Series, AFs: pd.Series) -> pd.Series:
     new Series safe for passing to the prediction model (i.e. with variants
     not used by the model removed and with AF values for missing variants).
     """
-    keep = set(vars.index).intersection(AFs.index)
-    missing = set(AFs.index) - set(vars.index)
+    keep = list(set(vars.index).intersection(AFs.index))
+    missing = list(set(AFs.index) - set(vars.index))
     new_vars = pd.concat((vars[keep], AFs[missing]))
     # ensure correct order
     new_vars = new_vars[AFs.index]
     return new_vars
 
 
-def run_prediction_container(container_path: str, vars: pd.Series) -> bool:
-    pass
-
+def run_prediction_container(pred_container_tar_path: str,
+                             vars: pd.Series) -> bool:
+    # get the image tag first
+    p = subprocess.run(['docker', 'load', '-i', pred_container_tar_path],
+                       capture_output=True, text=True)
+    img_name = p.stdout.split('Loaded image: ')[1].strip()
+    # now run the container got the prediction
+    p = subprocess.run(['docker', 'run', '-i', img_name], capture_output=True,
+                       text=True, input=vars.to_csv())
+    if p.returncode:
+        print('ERROR predicting resistance status '
+              f'with \'{pred_container_tar_path}\':')
+        print(p.stderr)
+        exit(1)
+    return bool(p.stdout)
