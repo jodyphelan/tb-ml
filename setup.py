2d8df240f17d6689930c56a2f30fd65ad7d15637
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-04-01 18:19:01 +0100

    fixed packaging

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import setuptools
 
-version = [l.strip() for l in open("tb_ml/__init__.py") if
-           "version" in l][0].split('"')[1]
+with open("tb_ml/__init__.py") as f:
+    version = [l.strip() for l in f if "version" in l][0].split('"')[1]
 
 setuptools.setup(
-
     name="tb-ml",
     version=version,
     packages=["tb_ml"],
     license="GPL3",
     long_description="A standardised approach to predicting resistance using machine learning",
-    scripts=[
-        'scripts/tb-ml-predict.py'
-    ]
+    entry_points={
+        "console_scripts": [
+            'tb-ml = tb_ml.__main__:main',
+        ]
+    },
 )
---
+++ tb_ml/__main__.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import tb_ml
+
+
+def main() -> None:
+    """
+    Test what we got so far using hardcoded filenames.
+    """
+    af_fname = '/home/julian/git/tb-ml/test_data/SM_training_means.csv'
+    bam_fname = '/home/julian/git/tb-ml/test_data/test.cram'
+    ref_fname = '/home/julian/git/tb-ml/test_data/MTB-h37rv_asm19595v2-eg18.fa'
+
+    AFs: pd.Series = pd.read_csv(af_fname, index_col=0).squeeze()
+    pos: list[int] = tb_ml.get_positions(AFs)
+    #     'test_data/INH_ERR1035006.csv', pos)
+    # vars = get_genotypes()
+    variants = tb_ml.get_genotypes(bam_fname, ref_fname, AFs)
+    docker_img_fname = ('/home/julian/git/tb-ml/docker/simple_RF_predictor/'
+                        'rf-sm-predictor.docker.tar.gz')
+    resistance_status: bool = tb_ml.run_prediction_container(
+        docker_img_fname, variants)
+    if resistance_status:
+        print('resistant')
+    else:
+        print('not resistant')
+
+
+if __name__ == '__main__':
+    main()
