2e37c65aac2502fd2e1ccd972fc628e7652bae5e
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-04-05 01:17:42 +0100

    skip \`samtools view\`

--- docker/freebayes_VC_pipeline/get_genotypes.sh
+++ docker/freebayes_VC_pipeline/get_genotypes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 af_fname=AFs.csv
-bam_fname=aligned_reads
+bam_fname=$1
 
 # write relevant positions to a dummy VCF and a dummy BED file
 cp vcf_header.txt vars.vcf
@@ -14,14 +14,10 @@ sed '1d' "$af_fname" | tr '_' ',' | awk -F',' \
     {print "Chromosome", $1-1, $1, ".", ".", $2, $3, ".", "AF=1"}' |
     sort -k2,2 -n > vars.bed
 
-# extract the reads overlapping with the variants of interest
-# (index the BAM first)
-samtools index "$bam_fname"
-samtools view -bML vars.bed "$bam_fname" > extracted.bam
-
+echo bloack
 # now run freebayes and format the output
-freebayes -f refgenome.fa extracted.bam \
+time freebayes -f refgenome.fa "$bam_fname" \
     --variant-input vars.vcf \
     --only-use-input-alleles |
     bcftools norm -f refgenome.fa -m - |
-    bcftools query -f '%POS\_%REF\_%ALT\t[%GT\t%DP]\n'
+    bcftools query -f '%POS\_%REF\_%ALT\t[%GT\t%DP]\n' > /dev/null
