c908d566089f84341cf8b6f4daf80f7c0ff3299e
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-04-05 10:55:28 +0100

    fix VC pipeline by omitting samtools view step

--- docker/freebayes_VC_pipeline/get_genotypes.sh
+++ docker/freebayes_VC_pipeline/get_genotypes.sh
@@ -3,25 +3,16 @@
 af_fname=AFs.csv
 bam_fname=$1
 
-# write relevant positions to a dummy VCF and a dummy BED file
+# write relevant positions to a dummy VCF
 cp vcf_header.txt vars.vcf
 sed '1d' "$af_fname" | tr '_' ',' | awk -F',' \
     'BEGIN{OFS="\t"}
     {print "Chromosome", $1, ".", $2, $3, ".", ".", "AF=1"}' |
     sort -k2,2 -n >> vars.vcf
-sed '1d' "$af_fname" | tr '_' ',' | awk -F',' \
-    'BEGIN{OFS="\t"}
-    {print "Chromosome", $1-1, $1, ".", ".", $2, $3, ".", "AF=1"}' |
-    sort -k2,2 -n > vars.bed
 
-# extract the reads overlapping with the variants of interest
-# (index the BAM first)
-# samtools index "$bam_fname"
-# time samtools view -bML vars.bed "$bam_fname" > extracted.bam
-echo bluuxx
 # now run freebayes and format the output
 time freebayes -f refgenome.fa "$bam_fname" \
     --variant-input vars.vcf \
     --only-use-input-alleles |
     bcftools norm -f refgenome.fa -m - |
-    bcftools query -f '%POS\_%REF\_%ALT\t[%GT\t%DP]\n' > /dev/null
+    bcftools query -f '%POS\_%REF\_%ALT\t[%GT\t%DP]\n'
