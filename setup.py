cbd8f9a0bdab20853bb5daab1dd33f7ade1db5ba
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-04-08 22:53:51 +0100

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
 import setuptools
 
-with open("tb_ml/__init__.py") as f:
-    version = [line.strip()
-               for line in f if "version" in line][0].split('"')[1]
 
-setuptools.setup(
-    name="tb-ml",
-    version=version,
-    packages=["tb_ml"],
-    license="GPL3",
-    long_description="A standardised approach to predicting resistance using' \
-         machine learning",
-    entry_points={
-        "console_scripts": [
-            'tb-ml = tb_ml.__main__:main',
-        ]
-    },
-)
+if __name__ == "__main__":
+    setuptools.setup()
