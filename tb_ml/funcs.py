f7b6741e642692c815ed56923e8077080144c34f
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-04-18 01:10:11 +0200

    add class for handling docker images and refactor funcs.py

--- tb_ml/funcs.py
+++ tb_ml/funcs.py
@@ -3,12 +3,58 @@ from typing import Optional
 import subprocess
 import argparse
 import io
-import os
 import sys
 
+from . import util
 
 DEFAULT_VC_CONTAINER = "julibeg/tb-ml-variant-calling:v0.1.1"
 DEFAULT_PRED_CONTAINER = "julibeg/tb-ml-streptomycin-rf-predictor:v0.1.1"
+
+
+class VariantCallingContainer(util.DockerImage):
+    def run_vc_pipeline(
+        self,
+        bam_file: str,
+        target_vars_AF: pd.Series,
+    ) -> tuple[pd.Series, pd.Series]:
+        bam_path = util.get_absolute_path(bam_file)
+        result = self.run(
+            docker_args=[
+                "--mount",
+                f"type=bind,source={bam_path},target=/data/aligned_reads",
+            ],
+            input=target_vars_AF.to_csv(),
+        )
+        # the first few lines of the result will start with '#' and hold some basic
+        # stats about the variant calling process
+        stats_lines = []
+        for line in result.split("\n"):
+            if line.startswith("#"):
+                stats_lines.append(line[1:])
+            else:
+                break
+        stats: pd.Series = pd.read_csv(
+            io.StringIO("\n".join(stats_lines)), index_col=0
+        ).squeeze()
+        variants: pd.Series = pd.read_csv(
+            io.StringIO(result),
+            index_col=["POS", "REF", "ALT"],
+            comment="#",
+        ).squeeze()
+        return stats, variants
+
+
+class PredictionContainer(util.DockerImage):
+    def get_target_variants_and_AFs(self) -> pd.Series:
+        target_vars: pd.Series = (
+            pd.read_csv(io.StringIO(self.run(extra_args=["get_target_vars"])))
+            .set_index(["POS", "REF", "ALT"])
+            .squeeze()
+        )
+        return target_vars
+
+    def predict(self, variants: pd.Series) -> float:
+        return float(self.run(input=variants.to_csv(), extra_args=["predict"]).strip())
 
 
 def get_cli_args() -> tuple[str, str, str, Optional[str], Optional[str]]:
@@ -66,8 +112,8 @@ def get_cli_args() -> tuple[str, str, str, Optional[str], Optional[str]]:
 
 def get_prediction(
     bam_file: str,
-    vc_container: str,
-    pred_container: str,
+    vc_img_name: str,
+    pred_img_name: str,
     write_vars_fname: Optional[str] = None,
 ) -> pd.Series:
     """
@@ -76,132 +122,27 @@ def get_prediction(
     reads of a sample
     (other input types will be added in the future).
     """
+    # pull / update the images and initialise the Docker objects
+    vc_container: VariantCallingContainer = VariantCallingContainer(vc_img_name)
+    pred_container: PredictionContainer = PredictionContainer(pred_img_name)
     # get the allele frequencies of the training dataset from the prediction container
-    target_vars_AF: pd.Series = get_target_vars_from_prediction_container(
-        pred_container
-    )
+    target_vars_AF: pd.Series = pred_container.get_target_variants_and_AFs()
     # run the variant calling container and provide the target variants so that it can
     # make sure that they are covered in the output
     vc_stats: pd.Series
     variants: pd.Series
-    vc_stats, variants = run_VC_container(bam_file, vc_container, target_vars_AF)
+    vc_stats, variants = vc_container.run_vc_pipeline(bam_file, target_vars_AF)
     if write_vars_fname is not None:
         variants.to_csv(write_vars_fname)
     # generate Series for final report
     res = pd.Series(dtype=object)
     res.index.name = "parameter"
     res.name = "value"
-    res["file"] = get_absolute_path(bam_file)
-    res["vc_container"] = vc_container
-    res["pred_container"] = pred_container
+    res["file"] = util.get_absolute_path(bam_file)
+    res["vc_container"] = vc_img_name
+    res["pred_container"] = pred_img_name
     res = pd.concat((res, vc_stats))
     # predict
-    res["resistance_probability"] = run_prediction_container(pred_container, variants)
+    res["resistance_probability"] = pred_container.predict(variants)
     res["resistance_status"] = "S" if res["resistance_probability"] < 0.5 else "R"
     return res
-
-
-def get_absolute_path(path: str) -> str:
-    """
-    Gets the absolute path and is aware of `$HOME` and `~/`
-    """
-    return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
-
-
-def run_VC_container(
-    bam_file: str,
-    VC_container_img_name: str,
-    target_vars_AF: pd.Series,
-    DP_threshold: int = 10,
-) -> tuple[pd.Series, pd.Series]:
-    """
-    Run a docker container containing a variant-calling pipeline and return the found
-    variants in a `pd.Series`. The container expects the target variants in the format
-    'POS,REF,ALT' in STDIN in order to make sure that they are covered in the results.
-    """
-    # run the container (the bind volume needs absolute paths)
-    bam_path = get_absolute_path(bam_file)
-    p = subprocess.run(
-        [
-            "docker",
-            "run",
-            "-i",
-            "--mount",
-            f"type=bind,source={bam_path},target=/data/aligned_reads",
-            VC_container_img_name,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=sys.stderr,
-        text=True,
-        input=target_vars_AF.to_csv(),
-    )
-    if p.returncode:
-        print(f"ERROR running variant-calling pipeline with '{VC_container_img_name}'")
-        sys.exit(p.returncode)
-    # the first few lines of the result will start with '#' and hold some basic stats
-    # about the variant calling process
-    stats_lines = []
-    for line in p.stdout.split("\n"):
-        if line.startswith("#"):
-            stats_lines.append(line[1:])
-        else:
-            break
-    stats: pd.Series = pd.read_csv(
-        io.StringIO("\n".join(stats_lines)), index_col=0
-    ).squeeze()
-    variants: pd.Series = pd.read_csv(
-        io.StringIO(p.stdout),
-        index_col=["POS", "REF", "ALT"],
-        comment="#",
-    ).squeeze()
-    return stats, variants
-
-
-def get_target_vars_from_prediction_container(
-    pred_container_img_name: str,
-) -> pd.Series:
-    """
-    Run the prediction container with "get_AFs" as argument in order to get the allele
-    frequencies of the training dataset. These can then be used for targeted variant
-    calling.
-    """
-    p = subprocess.run(
-        ["docker", "run", pred_container_img_name, "get_target_vars"],
-        stdout=subprocess.PIPE,
-        stderr=sys.stderr,
-        text=True,
-    )
-    if p.returncode:
-        print(f"ERROR extracting allele frequencies from '{pred_container_img_name}'")
-        sys.exit(p.returncode)
-    target_vars: pd.Series = (
-        pd.read_csv(io.StringIO(p.stdout)).set_index(["POS", "REF", "ALT"]).squeeze()
-    )
-    # make sure we actually got a Series
-    assert isinstance(target_vars, pd.Series), (
-        f"Prediction container {pred_container_img_name} returned "
-        "ill-formatted target variants."
-    )
-    return target_vars
-
-
-def run_prediction_container(
-    pred_container_img_name: str,
-    variants: pd.Series,
-) -> float:
-    """
-    Run a docker container containing a model for predicting AMR resistance from a
-    `pd.Series` of genotypes. The result is the probability of resistance.
-    """
-    # run the container to get the prediction
-    p = subprocess.run(
-        ["docker", "run", "-i", pred_container_img_name, "predict"],
-        stdout=subprocess.PIPE,
-        stderr=sys.stderr,
-        text=True,
-        input=variants.to_csv(),
-    )
-    if p.returncode:
-        print(f"ERROR predicting resistance status with '{pred_container_img_name}'")
-        sys.exit(p.returncode)
-    return float(p.stdout.strip())
---
+++ tb_ml/util.py
@@ -0,0 +1,80 @@
+import subprocess
+import sys
+from typing import Optional
+import os
+
+
+def get_absolute_path(path: str) -> str:
+    """
+    Gets the absolute path and is aware of `$HOME` and `~/`
+    """
+    return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+
+
+class DockerError(Exception):
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
+
+
+class DockerImage:
+    def __init__(self, img_name: str) -> None:
+        # check if the image name has a tag. Add 'latest' if not
+        img_name = (f"{img_name}:latest") if ":" not in img_name else img_name
+        self.img_name: str = img_name
+        # pull from the repo, if the image is already present on the host and
+        # up to date, `docker pull` won't do anything.
+        self.pull()
+
+    def pull(self) -> None:
+        """
+        Pull the image from the repo. If the local version is already up to date,
+        `docker pull` won't do anything.
+        """
+        output: str = self.exec_cmd(
+            ["pull"],
+            error_msg=f'Failed pulling image "{self.img_name}"',
+        )
+        if "Status: Image is up to date" in output:
+            print(f'Local image "{self.img_name}" is up to date', file=sys.stderr)
+
+    def exec_cmd(
+        self,
+        docker_args: list[str],
+        extra_args: Optional[list[str]] = None,
+        input: Optional[str] = None,
+        error_msg: Optional[str] = None,
+    ) -> str:
+        # replace the default `None` with an empty list
+        extra_args = [] if extra_args is None else extra_args
+        # the container is supposed to read from STDIN --> make sure `-i` is in the
+        # arguments
+        if input is not None and "-i" not in docker_args:
+            docker_args = docker_args + ["-i"]
+        p = subprocess.run(
+            [
+                "docker",
+                *docker_args,
+                self.img_name,
+                *extra_args,
+            ],
+            input=input,
+            capture_output=True,
+            text=True,
+        )
+        if p.returncode:
+            raise DockerError(f"{error_msg}:\n{p.stderr}")
+        return p.stdout
+
+    def run(
+        self,
+        docker_args: Optional[list[str]] = None,
+        extra_args: Optional[list[str]] = None,
+        input: Optional[str] = None,
+        error_msg: Optional[str] = None,
+    ) -> str:
+        """
+        Thin wrapper around `.exec_cmd()` running a command with `docker run ...`
+        """
+        # make sure the docker command starts with `run`
+        docker_args = ["run"] if docker_args is None else ["run"] + docker_args
+        return self.exec_cmd(docker_args, extra_args, input, error_msg)
