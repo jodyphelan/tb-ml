5ed67eed9f29a01f928417ff1639b62c98dbf90c
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2022-06-26 19:29:29 +0200

    update sambamba test script

--- test-sambamba-into-neural-net-pipeline.sh
+++ test-sambamba-into-neural-net-pipeline.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-cont1="julibeg/tb-ml-one-hot-encoded-from-cram:v0.2.0"
-cont2="julibeg/tb-ml-neural-net-predictor-13-drugs:v0.3.0"
+proc_cont="julibeg/tb-ml-one-hot-encoded-from-cram:v0.3.0"
+pred_cont="julibeg/tb-ml-neural-net-predictor-13-drugs:v0.4.0"
 
 tb-ml \
-    --container $cont1 \
-    "-b tb_ml/tests/test_data/test.cram \
-        -r /home/julian/git/tb-ml-containers/one_hot_encode/dev/H37RV_loci_coords_from_table_in_paper.bed \
+    --container $pred_cont \
+        "--get-target-loci \
+        -o target-loci.csv" \
+    --container $proc_cont \
+        "-b tb_ml/tests/test_data/test.cram \
+        -r target-loci.csv \
         -o one-hot-seqs.csv" \
-    --container $cont2 \
-        one-hot-seqs.csv
+    --container $pred_cont \
+        "one-hot-seqs.csv"
